5e01c6871a36d9b29b386bb2bb5a18a579b81b03
Kunal Kishore <kishorekunal225@gmail.com>
2025-11-27 10:21:19 +0530

    Update node placeholder

--- node/node.py
+++ node/node.py
@@ -1,5 +1,5 @@
 # Storage node for Distributed File System (DFS)
 # Will store actual file data.
 
-if __name__ == "__main__":
-    print("DFS Node placeholder")
+print("[NODE] Placeholder - Will register to master soon")
+
