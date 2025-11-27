d075b203916756420d2f37c6807065b72fc450cb
Kunal Kishore <kishorekunal225@gmail.com>
2025-11-27 10:20:07 +0530

    Add basic master server communication
    Implemented a master server for the Distributed File System (DFS) that handles client connections and requests.

--- master/master.py
+++ master/master.py
@@ -1,5 +1,32 @@
 # Master server for Distributed File System (DFS)
 # Will handle metadata and coordination.
 
+import socket
+
+HOST = '127.0.0.1'
+PORT = 5000
+
+def start_master():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+
+    print(f"[MASTER] Running on {HOST}:{PORT}")
+
+    while True:
+        conn, addr = server_socket.accept()
+        print(f"[MASTER] Connected with {addr}")
+
+        data = conn.recv(1024).decode()
+        print(f"[MASTER] Received: {data}")
+
+        if data == "HELLO":
+            conn.send("WELCOME CLIENT".encode())
+        else:
+            conn.send("UNKNOWN REQUEST".encode())
+
+        conn.close()
+
 if __name__ == "__main__":
-    print("DFS Master server placeholder")
+    start_master()
+
