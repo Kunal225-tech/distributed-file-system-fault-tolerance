4c0cb58818311b3825882ac337ab05b174de6617
Kunal Kishore <kishorekunal225@gmail.com>
2025-11-28 14:22:52 +0530

    Refactor DFS client to use HTTP requests

--- client/client.py
+++ client/client.py
@@ -1,18 +1,57 @@
-# Client for Distributed File System (DFS)
-# Code will be added step by step.
+import requests
 
-import socket
+MASTER_URL = "http://127.0.0.1:5000"
 
-HOST = '127.0.0.1'
-PORT = 5000
+def show_menu():
+    print("\n===== DFS CLIENT MENU =====")
+    print("1. Show Active Nodes")
+    print("2. Upload File")
+    print("3. Download File")
+    print("4. Exit")
+    return input("Enter your choice: ")
 
-def list_nodes():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    s.send("LIST_NODES".encode())
-    print("[CLIENT] Active Nodes:", s.recv(1024).decode())
-    s.close()
+def show_nodes():
+    try:
+        res = requests.get(f"{MASTER_URL}/nodes")
+        print("\nActive Nodes:", res.json())
+    except:
+        print("⚠ Could not connect to Master!")
+
+def upload_file():
+    filepath = input("Enter file path to upload: ")
+    try:
+        with open(filepath, "rb") as f:
+            files = {"file": f}
+            res = requests.post(f"{MASTER_URL}/upload", files=files)
+            print(res.text)
+    except:
+        print("⚠ File not found!")
+
+def download_file():
+    filename = input("Enter filename to download: ")
+
+    try:
+        res = requests.get(f"{MASTER_URL}/download/{filename}")
+        if res.status_code == 200:
+            with open("output_" + filename, "wb") as f:
+                f.write(res.content)
+            print("✔ File downloaded successfully!")
+        else:
+            print("⚠ File not found in DFS!")
+    except:
+        print("⚠ Error contacting server!")
 
 if __name__ == "__main__":
-    list_nodes()
-
+    while True:
+        choice = show_menu()
+        if choice == "1":
+            show_nodes()
+        elif choice == "2":
+            upload_file()
+        elif choice == "3":
+            download_file()
+        elif choice == "4":
+            print("Bye!")
+            break
+        else:
+            print("Invalid Input!")
