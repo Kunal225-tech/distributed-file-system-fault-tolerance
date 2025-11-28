220bc0acd2e3de06f3adab8531d8a42ccb1ec15b
Kunal Kishore <kishorekunal225@gmail.com>
2025-11-28 13:12:43 +0530

    Implement threading for heartbeat and chunk server
    Added threading for heartbeat and chunk server functions.

--- node/node.py
+++ node/node.py
@@ -2,33 +2,43 @@
 # Will store actual file data.
 
 import socket
+import threading
+import os
 import time
 
-MASTER_HOST = '127.0.0.1'
+NODE_ID = input("Enter Node Name (Example: node1): ")
+
+MASTER_HOST = "127.0.0.1"
 MASTER_PORT = 5000
-NODE_ID = "node1"
+NODE_PORT = 6000 if NODE_ID == "node1" else (6001 if NODE_ID == "node2" else 6002)
 
-def register_node():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((MASTER_HOST, MASTER_PORT))
-    s.send(f"REGISTER_NODE:{NODE_ID}".encode())
-    print("[NODE] Register Response:", s.recv(1024).decode())
-    s.close()
+os.makedirs("node_storage", exist_ok=True)
 
-def send_heartbeat():
+def heartbeat():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((MASTER_HOST, MASTER_PORT))
     while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            s.connect((MASTER_HOST, MASTER_PORT))
-            s.send(f"REGISTER_NODE:{NODE_ID}".encode())
-        except:
-            print("[NODE] Cannot reach master!")
-        s.close()
-        time.sleep(5)
+        sock.send(NODE_ID.encode())
+        time.sleep(3)
 
-if __name__ == "__main__":
-    register_node()
-    print("[NODE] Heartbeats started...")
-    send_heartbeat()
+def chunk_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("127.0.0.1", NODE_PORT))
+    server.listen(5)
+    print(f"[{NODE_ID}] Chunk Server Running on port {NODE_PORT}")
+
+    while True:
+        conn, addr = server.accept()
+        data = conn.recv(1024).decode()
+        filename, chunk_index, chunk_data = data.split("||")
+        path = f"node_storage/{filename}_chunk{chunk_index}"
+        with open(path, "w") as f:
+            f.write(chunk_data)
+        print(f"[{NODE_ID}] Stored {path}")
+        conn.send("OK".encode())
+        conn.close()
+
+threading.Thread(target=heartbeat, daemon=True).start()
+threading.Thread(target=chunk_server).start()
 
 
